d29c2f1ddd0af556f19e2a5b2b40262e5a68edd6
Robert Haslinger <if20b189@technikum-wien.at>
2021-04-17 08:45:22 +0200

    Add project files.

---
+++ Jobs/HelloWorldJob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace ScheduledNotifications.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class HelloWorldJob : IJob
+    {
+        private readonly ILogger<HelloWorldJob> _logger;
+
+        public HelloWorldJob(ILogger<HelloWorldJob> logger)
+        {
+            _logger = logger;
+        }
+        
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Hello World!");
+            return Task.CompletedTask;
+        }
+    }
+}
